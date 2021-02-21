a78d5771729709a3d01210091bca2f0c96beba11
Anurag Das <anurag.das23.ad@gmail.com>
2021-02-20 21:25:58 -0500

    Update tour by ID

--- app.js
+++ app.js
@@ -49,6 +49,26 @@ app.get('/api/v1/tours/:id', (req, res)=> {
     });
 });
 
+app.patch('/api/v1/tours/:id', (req, res)=> {
+
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    if (!tour) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid ID"
+        });
+    }
+    
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour: "<Updated tour here...>"
+        }
+    });
+});
+
 app.post('/api/v1/tours', (req, res)=> {
 //    console.log(req.body);
 
