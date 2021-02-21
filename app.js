bf6db922882a25bb160cf3e5c0ce0f1b832f24a5
Anurag Das <anurag.das23.ad@gmail.com>
2021-02-20 19:30:55 -0500

    Handling POST requests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 /*
 app.get('/', (req, res)=> {
@@ -25,6 +26,23 @@ app.get('/api/v1/tours', (req, res)=> {
         data: {
             tours
         }
+    });
+});
+
+app.post('/api/v1/tours', (req, res)=> {
+//    console.log(req.body);
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId}, req.body);
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: "success",
+            data: {
+                tour: newTour
+            }
+        });
     });
 });
 
