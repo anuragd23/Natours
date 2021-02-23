830e9f9e737a845f7e068c56e9e868118fbd626c
Anurag Das <anurag.das23.ad@gmail.com>
2021-02-22 20:10:12 -0500

    Chaining Middleware Methods

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,11 +5,12 @@ const tourController = require('../controllers/tourController');
 const Router = express.Router();
 
 Router.param('id', tourController.checkID);
+//Router.route('/').post(tourController.checkBody);
 
 Router
     .route('/')
     .get(tourController.getAllTours)
-    .post(tourController.createTour);
+    .post(tourController.checkBody, tourController.createTour);
 
 Router
     .route('/:id')
