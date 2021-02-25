b37a7068d4f34b40516bb8be97267b5679263157
Anurag Das <anurag.das23.ad@gmail.com>
2021-02-24 19:16:58 -0500

    Updating eslint rules

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 const express = require('express');
 const morgan = require('morgan');
 
@@ -8,16 +9,15 @@ const app = express();
 
 //1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
-
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
 // 3) ROUTES
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+/* eslint-disable prettier/prettier */
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env'});
 
 const app = require('./app');
-//console.log(process.env);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
