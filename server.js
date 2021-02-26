312711270fc4fe04b6d64e4283b53cdd75cd6645
Anurag Das <anurag.das23.ad@gmail.com>
2021-02-25 22:03:56 -0500

    Setting up DB connection

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 /* eslint-disable prettier/prettier */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env'});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("DB connected successfully!");
+//    console.log(con.connections);
+})
 
 const app = require('./app');
 
