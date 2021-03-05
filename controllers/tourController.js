5cb448c3660d3829a2e7ed25352393aae1e6ed8d
Anurag Das <anurag.das23.ad@gmail.com>
2021-03-05 18:25:08 -0500

    Added filter to get all tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,7 +9,13 @@ const tours = JSON.parse(
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
