9dd14032bb579fdbc32960dad4d9d94a57c2f771
Anurag Das <anurag.das23.ad@gmail.com>
2021-03-17 19:12:41 -0400

    Adding Aggregate middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -99,6 +99,12 @@ tourSchema.post(/^find/, function (docs, next) {
   next();
 });
 
+// AGGREGATION MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
