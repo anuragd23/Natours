b8a8ac4389d50c6ac2b47e797897febabc4990e5
Anurag Das <anurag.das23.ad@gmail.com>
2021-03-18 19:23:51 -0400

    Adding Built-In Validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A Tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have equal or more than 40 characters'],
+      minlength: [10, 'A tour name must have equal or more than 10 characters'],
     },
     slug: String,
     duration: {
@@ -21,10 +23,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A Tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      max: [5, 'Rating must be below 6'],
+      min: [1, 'Rating must be above 0'],
     },
     ratingsQuantity: {
       type: Number,
